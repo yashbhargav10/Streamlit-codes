fe04511ee7a612df40cd377f2ae57d9e136127d4
yashbhargav10 <77137155+yashbhargav10@users.noreply.github.com>
2022-01-04 20:58:08 +0530

    4 January initial commit

--- Streamlit learning codes.py
+++ Streamlit learning codes.py
@@ -80,3 +80,46 @@ if st.checkbox("Show/Hide"):
 #Working with beta expander
 with st.beta_expander("Python"):
 	st.text("Hello Python")
+	
+#Select/multiple select
+my_lang = ["Python", "Julia", "Go", "Rust"]
+
+choice = st.selectbox("Language", my_lang)
+st.write("You Selected {}".format(choice))
+
+#Multiple Selection
+spoken_lang = ("English","French","Spanish","Twi")
+my_spoken_lang = st.multiselect("Spoken lang", spoken_lang, default="English")
+
+
+#Slider
+#Numbers(Int/Float/Dates)
+age = st.slider("Age", 1,100,)
+
+#Any Datatype
+#Select Slider
+color = st.select_slider("Choose Color", options=["Yellow", "red", "blue", "black", "white"], value=("Yellow","red"))
+
+
+
+
+#Text input
+fname = st.text_input("Enter Firstname")
+
+#Text input hide password
+password = st.text_input("Enter password", type = 'password')
+st.title(fname)
+
+
+#Text area
+message = st.text_area("Enter Message", height = 100)
+st.write(message)
+
+#number
+number = st.number_input("Enter Number", 1.0, 25.0)
+
+#Date input
+myappointment = st.date_input("Appointment")
+
+#Time input
+mytime = st.time_input("My Time")
